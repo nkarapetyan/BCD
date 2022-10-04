63bcc4ed8b349f73676c32cb08705332833b3cbf
Nare Karapetyan <nare.karapetyan.w@gmail.com>
2022-10-04 00:06:35 -0400

    src branch in BCD

--- build/utils/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /rcp_dev_code/riverine_coverage_code/bcd_tmp/cl_bcd/utils/tests
-# Build directory: /rcp_dev_code/riverine_coverage_code/bcd_tmp/cl_bcd/build/utils/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(utils_tests "/rcp_dev_code/riverine_coverage_code/bcd_tmp/cl_bcd/build/utils/tests/utils_tests")
